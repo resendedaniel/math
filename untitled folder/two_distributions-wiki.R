58f086f80a72cc0c38329d6eb08863bd71b31426
Daniel Resende <dresende@verios.com.br>
2015-03-09 09:29:11 -0300

    Folder name change

--- untitled folder/two_distributions-wiki.R
+++
@@ -1,7 +0,0 @@
-df <- data.frame(sd50=rnorm(30, mean=100, sd=50),
-                 sd10=rnorm(30, mean=100, sd=10))
-df <- melt(df)
-
-ggplot(df, aes(x=value, color=variable)) +
-    geom_density(aes(fill=variable), alpha=.5) + theme_bw() +
-    geom_vline(xintercept=100, linetype = "longdash")
