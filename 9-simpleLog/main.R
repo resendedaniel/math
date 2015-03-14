079a86a511b40a8823c970be92a82571414bf4df
Daniel Resende <dresende@verios.com.br>
2015-03-14 12:41:23 -0300

    Simple log

---
+++ 9-simpleLog/main.R
@@ -0,0 +1,11 @@
+animateLog <- function(n) {
+    sapply(n, function(n) {
+        print(n)
+        plot(log(1:n), pch=20)
+        plot(log(1:n), pch=20, log="x")
+        Sys.sleep(1/4)
+    })
+}
+par(mfrow=c(2,1))
+n <- 2^(0:20)
+animateLog(n)
