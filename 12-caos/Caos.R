8e4ece58ec8b4c5380ec01cab9d02723bb5c4c24
Daniel Resende <dresende@verios.com.br>
2015-03-19 23:10:13 -0300

    Caos

---
+++ 12-caos/Caos.R
@@ -0,0 +1,28 @@
+caos <- function(r) {
+        r <<- r
+        x <- numeric(100)
+        x[1] <- 0.1
+        
+        for(i in 2:length(x)) {
+                x[i] <- caosAlg(x[i-1])
+        }
+        
+        x
+}
+
+caosAlg <- function(x) {
+        r * x * (1 - x)
+}
+
+count <- 100
+sample <- seq(from=2.5, to=4, length.out=201)
+sapply(sample, function(x) {
+        count <<- count + 1
+        z <- round((count-100)/201 * 255)
+        print(z)
+#         png(file=paste("Caos-", count, ".png", sep=""), width=1200, height=627, bg = "transparent")
+        plot(caos(x), pch=20, axes=FALSE, ylab="", xlab="", #col=rgb(z,z,z),
+             main=paste("Caos #", round(x, digits=2), sep=""))
+        Sys.sleep(1/16)
+#         dev.off()
+})
