71946479f95c9ec8f21efa9a37c8ccdfcd300b3b
Daniel Resende <dresende@verios.com.br>
2015-03-26 23:09:27 -0300

    Sharpe

---
+++ 13-Sharpe/mainScript.R
@@ -0,0 +1,16 @@
+plot(log(1:100),
+     type="l",
+     main="Sharpe",
+     xlab="Risco",
+     ylab="Rentabilidade",
+     frame=F)
+
+
+h <- c(5,10,
+       45,50)
+sapply(h, function(x) {
+    y <- log(x)
+    segments(x, 0, y1=y)
+    segments(0, y, x1=x)
+    log(h)
+})
