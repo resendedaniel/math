efe7c42abe1b5e8927a40832c588f585c9179251
Daniel Resende <dresende@verios.com.br>
2015-03-26 23:34:39 -0300

    IDK

--- 12-caos/main.R
+++ 12-caos/main.R
@@ -20,9 +20,9 @@ sapply(sample, function(x) {
         count <<- count + 1
         z <- round((count-100)/201 * 255)
         print(z)
-        png(file=paste("img-caos-", count, ".png", sep=""), width=1200, height=627, bg = "transparent")
+#         png(file=paste("img/caos-", count, ".png", sep=""), width=1200, height=627, bg = "transparent")
         plot(caos(x), pch=20, axes=FALSE, ylab="", xlab="", #col=rgb(z,z,z),
              main=paste("Caos #", round(x, digits=2), sep=""))
         Sys.sleep(1/16)
-        dev.off()
+#         dev.off()
 })
--- 13-Sharpe/mainScript.R
+++ 13-Sharpe/mainScript.R
@@ -1,4 +1,6 @@
-plot(log(1:100),
+n <- 100
+ratio <- 10/log(100)
+plot(log(1:n) * ratio,
      type="l",
      main="Sharpe",
      xlab="Risco",
@@ -9,7 +11,7 @@ plot(log(1:100),
 h <- c(5,10,
        45,50)
 sapply(h, function(x) {
-    y <- log(x)
+    y <- log(x) * ratio
     segments(x, 0, y1=y, lty="dashed")
     segments(0, y, x1=x, lty="dashed")
     log(h)
