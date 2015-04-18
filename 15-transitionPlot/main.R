b0eed04467bb8239c0641c4fcac26bd7acdd2389
Daniel Resende <dresende@verios.com.br>
2015-04-18 10:44:11 -0300

    update

--- 15-transitionPlot/main.R
+++ 15-transitionPlot/main.R
@@ -2,7 +2,7 @@ transitionPlot <- function(seq) {
     count <- 101
     for(n in seq) {
         x <- 1:201 / n
-        y <- sin(x/n)
+#         y <- sin(x/n)
 #         y <- sin(log(log(x)/n))^2/sqrt(cos(x/3))
 #         y <- (sin(x)^2)/sqrt(cos(log(x)/4))
 #         y <- sin(2*x)/sqrt(sin(sin(x))^2)
@@ -15,7 +15,7 @@ transitionPlot <- function(seq) {
              pch=20,
              axes=FALSE,
              frame.plot=FALSE,
-             xlab="Take me out of here\nthis numbers are crazy",
+             xlab="Take me out of here",
              ylab="")
 #         dev.off()
         Sys.sleep(1/12)
