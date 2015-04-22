d380be27e20ed10dce2134e4590b4091a5a15daf
Daniel Resende <dresende@verios.com.br>
2015-04-22 20:40:31 -0300

    video instructions

--- 12-caos/main.R
+++ 12-caos/main.R
@@ -1,3 +1,7 @@
+# Convert to gif
+## ffmpeg -f image2 -i caos-%d.png ../video/video.avi
+## ffmpeg -i video.avi -pix_fmt rgb24 out.gif
+
 caos <- function(r) {
         r <<- r
         x <- numeric(100)
@@ -14,15 +18,13 @@ caosAlg <- function(x) {
         r * x * (1 - x)
 }
 
-count <- 100
-sample <- seq(from=2.5, to=4, length.out=201)
-sapply(sample, function(x) {
-        count <<- count + 1
-        z <- round((count-100)/201 * 255)
-        print(z)
-#         png(file=paste("img/caos-", count, ".png", sep=""), width=1200, height=627, bg = "transparent")
-        plot(caos(x), pch=20, axes=FALSE, ylab="", xlab="", #col=rgb(z,z,z),
-             main=paste("Caos #", round(x, digits=2), sep=""))
-        Sys.sleep(1/16)
-#         dev.off()
-})
+n <- 100
+r <- seq(from=2.5, to=4, length.out=n)
+mapply(function(r, i) {
+        func <- paste0("f(x) = ", round(r, digits=2), " * x * (1 - x)")
+        png(file=paste("img/caos-", i, ".png", sep=""), width=1200, height=627)#, bg = "transparent")
+        plot(caos(r), pch=20, axes=FALSE, ylab="", xlab="", #col=rgb(z,z,z),
+             main=paste("Chaos: ", func, sep=""))
+#         Sys.sleep(1/16)
+        dev.off()
+}, r=r, i=1:n)
