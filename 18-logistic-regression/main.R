5ff3c575c03925612f803b7d36efabf1c240447f
Daniel Resende <dresende@verios.com.br>
2015-05-02 17:28:53 -0300

    no frame

--- 18-logistic-regression/main.R
+++ 18-logistic-regression/main.R
@@ -5,6 +5,6 @@ logisticFunction <- function(x) {
 x <- seq(-6, 6, by=.1)
 y <- logisticFunction(x)
 
-png("img/img1.png")
-plot(x, f(x), pch=20, main="Logistic Regression")
-dev.off()
+# png("img/img1.png")
+plot(x, f(x), pch=20, main="Logistic Regression", frame=F)
+# dev.off()
