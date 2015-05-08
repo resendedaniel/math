73890d70cdeffb23be8f75298c85ae7f6e3bef6f
Daniel Resende <dresende@verios.com.br>
2015-05-08 18:48:44 -0300

    t test on title

--- 10-tTest/main.R
+++ 10-tTest/main.R
@@ -20,6 +20,8 @@ sigma1 <- sd(x1) / sqrt(n1)
 
 x <- range(c(m0 + c(-1, 1) * 4 * sigma0, m1 + c(-1, 1) * 4 * sigma1))
 
+t <- t.test(x0, x1)
+
 g <- ggplot(data.frame(x=seq(x[1], x[2], length.out=11)), aes(x)) +
     geom_histogram(data=data.frame(x0), aes(x0, y=..density..), fill="darkred", alpha=.5) +
     geom_histogram(data=data.frame(x1), aes(x1, y=..density..), fill="darkblue", alpha=.5) +
@@ -27,5 +29,9 @@ g <- ggplot(data.frame(x=seq(x[1], x[2], length.out=11)), aes(x)) +
                   args=list(mean=m0, sd=sigma0), col="darkred") +
     stat_function(fun=dnorm, geom="line",
                   args=list(mean=m1, sd=sigma1), col="darkblue") +
-    geom_vline(aes(xintercept=qnorm(.975, m0, sigma0)), col="darkred")
+    geom_vline(aes(xintercept=qnorm(.975, m0, sigma0)), col="darkred") +
+    ggtitle(round(1-t$p.value,2))
 g
+
+print(x0)
+print(x1)
