6c166598cd64e1b8168112d96a15f274645fab5d
Daniel Resende <dresende@verios.com.br>
2015-12-05 09:45:04 -0200

    improve charts

--- 26-maclaurin/main.R
+++ 26-maclaurin/main.R
@@ -1,4 +1,4 @@
-for(j in (0:20) * 2) {
+data <- sapply((0:20) * 2, function(j) {
     print(j)
     
     f <- function(x) {
@@ -13,16 +13,19 @@ for(j in (0:20) * 2) {
     tau <- 2*pi
     x <- seq(-2*tau, 2*tau, by=tau/2^5)
     y <- f(x)
-    g <- ggplot(data.frame(x, y), aes(x, y)) + geom_line() + ylim(-2, 2) +
-        geom_line(data=data.frame(x, y=cos(x)), aes(x, y), linetype="dashed") +
-        scale_x_continuous(breaks=(-4:4) * pi, labels=paste0(-4:4, "\u03C0")) +
-        ylab("cos") +
-        xlab(paste("Aproximation ", j, "degree")) +
-        ggtitle("Cosine Taylor series at 0 (Maclaurin)")
-    print(g)
-    png(paste0("img/", paste(c(rep("0", 4-nchar(as.character(j))), j), collapse=""), ".png"),
-        720, 400)
-    dev.off()
-    format(j, "")
-    Sys.sleep(.5)
-}
+    
+    data.frame(x, y, term=j)
+}, simplify=F)
+
+g <- ggplot(data, aes(x, y, colour=term)) + geom_line() + ylim(-2, 2) +
+    geom_line(data=data.frame(x, y=cos(x)), aes(x, y), linetype="dashed") +
+    scale_x_continuous(breaks=(-4:4) * pi, labels=paste0(-4:4, "\u03C0")) +
+    ylab("cos") +
+    xlab(paste("Aproximation ", j, "degree")) +
+    ggtitle("Cosine Taylor series at 0 (Maclaurin)")
+
+# png(paste0("img/", paste(c(rep("0", 4-nchar(as.character(j))), j), collapse=""), ".png"),
+#     720, 400)
+print(g)
+# dev.off()
+# Sys.sleep(.5)
